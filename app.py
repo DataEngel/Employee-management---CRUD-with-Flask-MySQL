8096063e4e6a0f4c261372d69d66043ad7c6a155
DataEngel <miguel.program.73@gmail.com>
2021-07-05 01:38:20 -0500

    Querying data from the employees table

--- app.py
+++ app.py
@@ -16,12 +16,15 @@ mysql.init_app(app)
 @app.route('/')
 def index():
 
-    sql = "INSERT INTO `empleados` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'Miguel', 'miguel@gmail.com', 'angel.jpg');"
+    sql = "SELECT * FROM  `empleados`;" 
     conn = mysql.connect()
     cursor = conn.cursor() 
     cursor.execute(sql) 
-    conn.commit()
+    
+    empleados=cursor.fetchall()
+    print(empleados) 
 
+    conn.commit()
     return render_template('empleados/index.html') 
 
 @app.route('/create')
